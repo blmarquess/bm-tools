bc04b71317b4ca88fb4b270c5f443a6709a6e659
Bruno Marques <blmarques.dev@gmail.com>
2024-11-21 02:42:58 -0300

    📦 build: add initial project files

---
+++ constants.ts
@@ -0,0 +1,6 @@
+export const NOT = ['n', 'no', 'N', 'No', 'NO', 'não', 'Não']
+export const YES = ['y', 'yes', 'Y', 'Yes', 'YES', 'sim', 'Sim']
+
+export const EXEC_COMMIT_QUESTION = 'Do you want to exec this commit(s)?'
+
+export const HAS_COPY_TO_CLIPBOARD = '::This message has copy to clipboard:'
